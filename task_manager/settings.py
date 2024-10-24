a33dce79585e2adc01c74646e86ad47e9ba315af
Evgeny <info@tinamagic.lv>
2024-10-24 18:30:01 +0300

    Step 1

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,10 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # DEBUG = os.getenv('DEBUG', 'False') == 'True'
 DEBUG = 'True'
 
-ALLOWED_HOSTS = [
-    '*',
-    'webserver',
-]
+ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
