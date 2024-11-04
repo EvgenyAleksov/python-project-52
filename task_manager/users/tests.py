6c2e088ac50620c7b26a3bf17e1ef439c62932a3
Evgeny <info@tinamagic.lv>
2024-11-04 19:15:02 +0200

    Step 3

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -17,8 +17,8 @@ class TestUsers(TestCase):
                 'first_name': 'T1',
                 'last_name': 'M1',
                 'username': 'TM1',
-                'password1': 'TM022008',
-                'password2': 'TM022008',
+                'password1': 'TM111111',
+                'password2': 'TM111111',
             },
         )
         self.assertEqual(response.status_code, 302)
@@ -32,10 +32,50 @@ class TestUsers(TestCase):
         self.assertTrue(len(response.context['users']) == 1)
 
     def test_update_user(self):
-        user = User.objects.get(id=1)
+        user = User.objects.get(id=8)
 
         response = self.client.get(
-            reverse('update_user', kwargs={'pk': user.id})
+            reverse('user_update', kwargs={'pk': user.id})
         )
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('login'))
+
+        self.client.force_login(user)
+        response = self.client.get(
+            reverse('user_update', kwargs={'pk': user.id})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='users/users.html')
+        response = self.client.post(
+            reverse('user_update', kwargs={'pk': user.id}),
+            {
+                'first_name': 'T1',
+                'last_name': 'M1',
+                'username': 'TM1',
+                'password1': 'TM111111',
+                'password2': 'TM111111',
+            }
+        )
+        self.assertEqual(response.status_code, 302)
+        user.refresh_from_db()
+        self.assertEqual(user.first_name, 'T1')
+
+    def test_DeleteUser(self):
+        user = User.objects.get(username='TM1')
+
+        response = self.client.get(
+            reverse('user_delete', kwargs={'pk': user.id}))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('login'))
+
+        self.client.force_login(user)
+        response = self.client.get(
+            reverse('user_delete', kwargs={'pk': user.id}))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(
+            reverse('user_delete', kwargs={'pk': user.id})
+        )
+        self.assertRedirects(response, reverse('users'))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(User.objects.count(), 1)
