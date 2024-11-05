142109c345d452b6fcc09b575ec16a247a53c0ce
Evgeny <info@tinamagic.lv>
2024-11-05 14:37:14 +0200

    Step 4

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-make install && psql -a -d $DATABASE_URL -f postgresql://python_project_52_2p4s_user:i72ZfjhAk84QCAIhLACfz5QqEPyUrnop@dpg-csd6u9lds78s73bkg6ag-a/python_project_52_2p4s
+make install && psql -a -d $DATABASE_URL -f
