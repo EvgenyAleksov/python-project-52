70ee05a368f3d38db2c8ab0205c9dd95e7f6445e
Evgeny <info@tinamagic.lv>
2024-11-06 19:08:47 +0200

    Step 4

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -92,12 +92,9 @@ DATABASES = {
     }
 }
 
+# if os.getenv('DATABASE_URL'):
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
-
-# if os.getenv('DATABASE_URL'):
-#     db_from_env = dj_database_url.config(conn_max_age=600)
-#     DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
