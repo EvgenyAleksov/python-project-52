b17fe09f0caafcb90abe4d1e00b583a62601983e
Evgeny <info@tinamagic.lv>
2024-11-18 13:56:42 +0200

    Step 8

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('logout/', views.LogoutUser.as_view(), name='logout'),
     path('statuses/', include('task_manager.statuses.urls')),
     path('tasks/', include('task_manager.tasks.urls')),
-    path('labels/', include('task_manager.labels.urls'))
+    path('labels/', include('task_manager.labels.urls')),
+    path('', views.index)
 ]
