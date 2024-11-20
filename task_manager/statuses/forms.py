daa2ed59a1fcdc3111b1ab1739bc50f7ac4fa65d
Evgeny <info@tinamagic.lv>
2024-11-20 20:40:21 +0200

    Step 8

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -1,9 +1,11 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, CharField
 
 from task_manager.statuses.models import Status
 
 
 class StatusForm(ModelForm):
+    name = CharField(label='Имя')
+
     class Meta:
         model = Status
         fields = ['name']
