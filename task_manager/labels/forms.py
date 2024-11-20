e6518676e486820f2cdb4bfc7320e2101561dd40
Evgeny <info@tinamagic.lv>
2024-11-20 21:07:22 +0200

    Step 8

--- task_manager/labels/forms.py
+++ task_manager/labels/forms.py
@@ -1,9 +1,11 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, CharField
 
 from task_manager.labels.models import Label
 
 
 class LabelForm(ModelForm):
+    name = CharField(label='Имя')
+
     class Meta:
         model = Label
         fields = ['name']
--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,9 +1,53 @@
-from django.forms import ModelForm
+from django import forms
+from django.forms import ModelForm, CharField
 
+from task_manager.users.models import User
 from task_manager.tasks.models import Task
+from task_manager.statuses.models import Status
+from task_manager.labels.models import Label
 
 
 class TaskForm(ModelForm):
+    name = CharField(label='Имя')
+
+    description = CharField(label='Описание')
+
+    status = forms.ModelChoiceField(
+        queryset=Status.objects.all(),
+        label='Статус',
+        widget=forms.Select(attrs={'class': 'form-control'}))
+
+    executor = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        label='Исполнитель',
+        widget=forms.Select(attrs={'class': 'form-control'}))
+
+    labels = forms.ModelMultipleChoiceField(
+        queryset=Label.objects.all(),
+        label='Метки',
+        widget=forms.SelectMultiple(attrs={'class': 'form-control'}),
+        required=False)
+
     class Meta:
         model = Task
         fields = ('name', 'description', 'status', 'executor', 'labels')
+        # widgets = {
+        #     'name': forms.TextInput(
+        #         attrs={
+        #             'placeholder': 'Имя',
+        #             'class': 'form-control'
+        #         }
+        #     ),
+        #     'description': forms.Textarea(
+        #         attrs={
+        #             'placeholder': 'Описание',
+        #             'cols': 40,
+        #             'rows': 10,
+        #             'class': 'form-control'
+        #         }
+        #     )
+        # }
+        # labels = {
+        #     'name': 'Имя',
+        #     'description': 'Описание'
+        # }
