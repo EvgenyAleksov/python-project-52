5ad9d2ba676e0ab07451fc8ca75c0558768115d6
Evgeny <info@tinamagic.lv>
2024-11-24 16:18:17 +0200

    Step 8

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -104,17 +104,21 @@ if os.getenv('DATABASE_URL'):
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-      'NAME':
-      'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.\
+        UserAttributeSimilarityValidator',
     },
     {
-      'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+        MinimumLengthValidator',
     },
     {
-      'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+        CommonPasswordValidator',
     },
     {
-      'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+        NumericPasswordValidator',
     },
 ]
 
