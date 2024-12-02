8cb47d8bdb8cdd28158eae50d957714acaffd3eb
Evgeny <info@tinamagic.lv>
2024-12-02 21:24:40 +0200

    Check

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import (ListView, CreateView,
-                                  UpdateView, DeleteView)
+from django.views.generic import (CreateView, DeleteView,
+                                  ListView, UpdateView)
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
