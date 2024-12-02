bc697ae3e51d5df20f56205c9d4b7222f28f6ed5
Evgeny <info@tinamagic.lv>
2024-12-02 21:30:45 +0200

    Check

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from task_manager.labels.views import (LabelListView, LabelCreateView,
-                                       LabelUpdateView, LabelDeleteView)
+from task_manager.labels.views import (LabelCreateView, LabelListView,
+                                       LabelDeleteView, LabelUpdateView)
 
 
 urlpatterns = [
